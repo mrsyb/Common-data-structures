b4f1f5e80635aa21ce6ab9947657b17aa80f64b0
Mr. shi <c_shiyibin@qq.com>
2019-07-16 15:18:01 +0800

    add graph pseudocode

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -42,6 +42,51 @@ void Graph<T, E>::BFS(Graph<T, E>& G, const T & v,void(*Visit)(T))
 	delete[] Visited;
 }
 
+template<class T, class E>
+void Graph<T, E>::Dijkstra(Graph<T, E>& G, T v, E dist[], int path[])
+{
+	int n = G.NumberOfVertices();
+	bool *S = new bool[n]; // 判断是否已存入该点到S集合中
+	int i, j, k;
+	E w, min;
+	for (i = 0; i < n; i++)
+	{
+		dist[i] = G.GetWeight(v, i);
+		S[i] = false;
+		if (i != v && dist[i] < INT_MAX)
+			path[i] = v;
+		else
+			path[i] = -1;
+	}
+
+	S[v] = true;
+	dist[v] = 0;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		min = INT_MAX;
+		int u = v;
+		for (j = 0; j < n; j++)
+		{
+			if (S[j] == false && dist[j] < min)
+			{
+				u = j;
+				min = dist[j];
+			}
+		}
+		S[u] = true;
+		for (k = 0; k < n; k++)
+		{
+			w = G.GetWeight(u, k);
+			if (S[k] == false && w < INT_MAX && dist[u] + w < dist[k])
+			{
+				dist[k] = dist[u] + w;
+				path[k] = u;
+			} 
+		}
+	}
+}
+
 
 template<class T, class E>
 void Graph<T, E>::DFS(Graph<T, E>& G, int v, bool Visited[], void(*Visit)(T))
--- Graph/Graph.h
+++ Graph/Graph.h
@@ -26,6 +26,35 @@ if(Vmst包含的顶点数小于n)
 	生成失败；
 */
 
+/*Dijkstra迪杰斯特拉算法<https://www.cnblogs.com/biyeymyhjob/archive/2012/07/31/2615833.html>
+提法：给定一个有向带权图G与源点V0，各边上的权值均非负，要求找出从v到D中其余各顶点的最短路径
+1.初始化：	S = {V0};								//S存放已求出 最短路径的终点
+			Dist[j] = Edge[0][j],j = 0,1,2...,n-1;	//Dist[j]表示当前已经找出来的从源点V0到Vj的最短路径的权值，n为带权无向图顶点的个数
+2.求出最短路径的长度：
+			Dist[k] = min{Dist[i]},i属于V-S			//V表示带权无向图所有顶的集合
+			S = S U {K};
+3.修改：	Dist[i] = min{Dist[k] + Edge[k][i]},对于每一个i属于V - S;
+4.判断：    若S = V ，则算法结束，否则转2；
+*/
+
+/*拓扑排序<https://blog.csdn.net/lisonglisonglisong/article/details/45543451>
+定义：拓扑排序是一个有向无环图所有顶点的线性序列。
+stack S;
+检查所有顶点；
+	if(顶点i的入度为0)
+		顶点号i进栈S;
+count = 0;
+while(当栈S不空时)
+{
+	从栈S退出一个顶点v;
+	输出v;
+	count++;
+	遍历顶点v的所有邻接顶点w,将其入度减一入度减至0的顶点w入栈S;
+}
+if(输出顶点个数count < 网络顶点个数n)
+	报告网络中存在有向环；
+*/
+
 #include <cstdlib>
 const int DefaultVetices = 30;
 
@@ -56,6 +85,7 @@ public:
 	virtual bool RemoveEdge(int v1, int v2);
 	void DFS(Graph<T, E>& G,const T& v, void(*Visit)(T));
 	void BFS(Graph<T, E>& G, const T& v,void(*Visit)(T));
+	void Dijkstra(Graph<T, E>& G, T v, E dist[], int path[]);
 protected:
 	int MaxVertices;
 	int NumEdgets;
